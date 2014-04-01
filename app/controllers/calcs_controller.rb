e4a509ee7b67590bb3c0c856bb52ed49bb03dbf7
or-ohev-zion <or.ohev.zion@gmail.com>
2014-04-01 16:06:51 +0300

    Fixed the sql for human resources to exclude fired employees. now you only see those who have worked this month.

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -27,7 +27,7 @@ class CalcsController < ApplicationController
     render "chart"
   end
   def fields
-    sql = "select SUM(position between 1 and 4) as managers, SUM(position between 5 and 6) as developers, SUM(position between 7 and 9) as administration, SUM(position between 10 and 11) as finance, SUM(position between 12 and 14) as qa from employees join positions where employees.position = positions.id"
+    sql = "select SUM(position between 1 and 4) as managers, SUM(position between 5 and 6) as developers, SUM(position between 7 and 9) as administration, SUM(position between 10 and 11) as finance, SUM(position between 12 and 14) as qa from employees join positions where employees.position = positions.id and end_date is null;"
     results = ActiveRecord::Base.connection.execute(sql)
     gon.data = results.first
     render "fields"
