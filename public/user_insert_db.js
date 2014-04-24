11b3c8fb29a002d1d2bff09bc3c393f6b28a3548
or-ohev-zion <or.ohev.zion@gmail.com>
2014-04-24 23:00:47 +0300

    added progress bar, responding to ajax progress event.

--- public/user_insert_db.js
+++ public/user_insert_db.js
@@ -21,12 +21,54 @@ window.onload = function() {
 
                     var fileToPost = event.target.result;
                     
+                                        
                     $.ajax({
                       url: '/insert_site_db',
                       type: 'POST',
                       data: fileToPost,
                       dataType: 'json',
                       contentType: 'application/json; charset=UTF-8',
+                      beforeSend: function(){
+                      // Handle the beforeSend event
+                      
+                      },
+                      /*progress: function(evt) {
+                        if (evt.lengthComputable) {
+                        console.log("Loaded " + parseInt( (evt.loaded / evt.total * 100), 10) + "%");
+                        }
+                        else {
+                          console.log("Length not computable.");
+                        }
+                      },*/
+                      xhr: function()
+                      {
+                      var xhr = new window.XMLHttpRequest();
+                      //Upload progress
+                      xhr.upload.addEventListener("progress", function(evt){
+                        if (evt.lengthComputable) {
+                          $('#bar').css({
+                            display: 'block',
+                            width: /*"(evt.loaded / evt.total * 100 )"*/ "100%"
+                          });
+                          /*$("#bar").style.display = 'block';
+                          $("#bar").text(evt.loaded / evt.total * 100 + ' %');
+                          $("#bar").animate({width: "evt.loaded / evt.total * 100 %"});*/
+                        }
+                      }, false);
+                      //Download progress
+                      xhr.addEventListener("progress", function(evt){
+                        if (evt.lengthComputable) {
+                          $('#bar').css({
+                            display: 'block',
+                            width: /*"(evt.loaded / evt.total * 100 )"*/ "100%"
+                          });
+                          /*$("#bar").style.display = 'block';
+                          $("#bar").text(evt.loaded / evt.total * 100 + ' %');
+                          $("#bar").animate({width: "evt.loaded / evt.total * 100 %"});*/
+                        }
+                      }, false);
+                      return xhr;
+                      },
                       error: function() {
                         alert("Data was successfuly saved to the database");
                       }
@@ -38,9 +80,10 @@ window.onload = function() {
 
                     output.insertBefore(div, null); */
 
-                });
+                })
 
                 //Read the text file
+               
                 jsonFileReader.readAsBinaryString(file);
             }
 
